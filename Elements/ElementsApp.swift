afaac3f4f1d0d338895288315b44a61baee9105a
Snaggle <rwaalders@gmail.com>
2023-08-11 09:04:41 +0200

    Initial Commit

---
+++ Elements/ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsApp.swift
+//  Elements
+//
+//  Created by Rick Waalders on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
